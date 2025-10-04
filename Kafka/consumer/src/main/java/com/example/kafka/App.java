7c9ce3e257bb17a26fa661fe91acce312b1dc97b
AhmedElshewemy <ahmedelshewemy11@gmail.com>
2025-10-04 22:23:08 +0300

    init kafka consumer

--- Kafka/consumer/src/main/java/com/example/kafka/App.java
+++ Kafka/consumer/src/main/java/com/example/kafka/App.java
@@ -1,13 +1,24 @@
 package com.example.kafka;
 
+import java.util.Properties;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
+    private static String kafkaHost = "localhost:9092";
+    private static String topic = "fancy-topic";
+    private static String groupName = "fancy-group";
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        Properties properties = new Properties();
+        properties.put("bootstrap.servers", kafkaHost);
+        properties.put("group.id", groupName);
+        properties.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+        properties.put("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+        KafkaConsumer<String, String> consumer = new KafkaConsumer<>(properties);
     }
 }
